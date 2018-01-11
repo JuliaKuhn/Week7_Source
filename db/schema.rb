9665ad2765e18bf32be04e32aadf9bd33443bfb2
Rik Lomas <lomas.rik@gmail.com>
2018-01-11 15:14:52 -0500

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110072924) do
+ActiveRecord::Schema.define(version: 20180111101711) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
