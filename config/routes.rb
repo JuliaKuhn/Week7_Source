7e79407fe060c0fc2231e395aeddfcaae91ff645
Julia Kuhn <juliakuhn@me.com>
2018-01-15 16:25:48 +0100

    set up categories model

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-resources :items 
+  resources :categories
 
+  resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
